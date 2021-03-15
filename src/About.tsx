1660c2fca06bd94d7541e87b0a4065a608880f4e
ryo-i <iidazz0109@gmail.com>
2021-03-16 06:58:44 +0900

    input type=rangeタグ追加

--- src/About.tsx
+++ src/About.tsx
@@ -9,10 +9,10 @@ function About() {
           <h2>イイダリョウ</h2>
           <p>フロントエンドエンジニア。神奈川に住まう四十路のオジキ。 DTP→Webデザイナーから転向し今に至る。引き続きコツコツの日々。ブログも書いてます。 Webづくり やりたい時が 始め時！</p>
           <ul>
-              <li><a href='https://www.i-ryo.com'>ブログ</a></li>
-              <li><a href='https://twitter.com/idr_zz'>Twitter</a></li>
-              <li><a href='https://qiita.com/i-ryo'>Qiita</a></li>
-              <li><a href='https://github.com/ryo-i'>GitHub</a></li>
+              <li><a href="https://www.i-ryo.com">ブログ</a></li>
+              <li><a href="https://twitter.com/idr_zz">Twitter</a></li>
+              <li><a href="https://qiita.com/i-ryo">Qiita</a></li>
+              <li><a href="https://github.com/ryo-i">GitHub</a></li>
            </ul>
         </section>
     </div>
--- src/Inner.tsx
+++ src/Inner.tsx
@@ -1,25 +1,30 @@
-import React, { useEffect }  from 'react';
-import { hello } from './modules/hello/hello';
-import Data from './data/data.json';
+import React  from 'react';
+import styled from 'styled-components';
 
 
-const innerJson = Data.data.inner;
+// Style
+const InputRange = styled.input`
+  width: 50%;
+`;
 
 
 // Component
 function Inner() {
-  useEffect(() => {
-    hello();
-  });
 
   return (
     <div className="inner">
-      {innerJson.map((innerJson, index) =>
         <section>
-          <h2>{ innerJson.title }</h2>
-          <p dangerouslySetInnerHTML={{ __html: innerJson.text }}></p>
+          <h2>行長</h2>
+          <InputRange type="range" name="range" min="10" max="50" value="35"></InputRange>
         </section>
-      )}
+        <section>
+          <h2>行間</h2>
+          <InputRange type="range" name="range" min="1" max="2.5" value="1.75" step="0.01"></InputRange>
+        </section>
+        <section>
+          <h2>ジャンプ率</h2>
+          <InputRange type="range" name="range" min="100" max="400" value="200"></InputRange>
+        </section>
     </div>
   );
 }
