fa98d5dffa62198d70ca053d9022e4b53a60384d
ryo-i <iidazz0109@gmail.com>
2021-03-29 06:40:21 +0900

    下のツマミも動かす

--- src/Inner.tsx
+++ src/Inner.tsx
@@ -17,11 +17,25 @@ const test = (e: React.ChangeEvent<HTMLInputElement>) => {
 function Inner() {
 
   const [lineLength, setLineLength] = useState(35);
+  const [lineHeight, setLineHeight] = useState(1.75);
+  const [jumpRate, setJumpRate] = useState(200);
 
   const changelineLength = (e: React.ChangeEvent<HTMLInputElement>) => {
     let changeValue: number = Number(e.target.value);
     console.log(changeValue);
     setLineLength(changeValue);
+  };
+
+  const changeLineHeight = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let changeValue: number = Number(e.target.value);
+    console.log(changeValue);
+    setLineHeight(changeValue);
+  };
+
+  const changeJumpRate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let changeValue: number = Number(e.target.value);
+    console.log(changeValue);
+    setJumpRate(changeValue);
   };
 
   return (
@@ -33,11 +47,13 @@ function Inner() {
         </section>
         <section>
           <h2>行間</h2>
-          <InputRange type="range" name="range" min="1" max="2.5" value="1.75" step="0.01" onChange={test}></InputRange>
+          <p>値：{lineHeight}</p>
+          <InputRange type="range" name="range" min="1" max="2.5" defaultValue={lineHeight} onChange={changeLineHeight} step="0.01"></InputRange>
         </section>
         <section>
           <h2>ジャンプ率</h2>
-          <InputRange type="range" name="range" min="100" max="400" value="200" onChange={test}></InputRange>
+          <p>値：{jumpRate}</p>
+          <InputRange type="range" name="range" min="100" max="400" defaultValue={jumpRate} onChange={changeJumpRate}></InputRange>
         </section>
     </div>
   );
